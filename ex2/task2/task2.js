61f479f3f201fd843d8f7013b7b4a3be84b83c7b
vkahara <valtterikahara@gmail.com>
2023-02-07 19:41:08 +0200

    lisatty ex2 tehtavat 1-3

---
+++ ex2/task2/task2.js
@@ -0,0 +1,7 @@
+const containsNumber = function(numbers, aNumber) {
+
+    const includesNumber = numbers.includes(aNumber);
+    console.log(includesNumber);
+};
+
+containsNumber([6,4,2,5,9,7,5,7,2], 5)
